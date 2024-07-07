d38b86f157bde07f61bbbef30f8bc7215dadebd4
Gavin Eadie <gavin@umich.edu>
2024-07-06 20:51:58 -0400

    Initial Commit

---
+++ DSKY/DSKYApp.swift
@@ -0,0 +1,17 @@
+//
+//  DSKYApp.swift
+//  DSKY
+//
+//  Created by Gavin Eadie on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct DSKYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
