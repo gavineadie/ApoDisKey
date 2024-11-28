78f5ca9c5f69d0040c637f3dd145cbe4c2aed976
Gavin Eadie <gavin@umich.edu>
2024-11-28 00:27:38 -0500

    add a "Previews" file and put the #Previews in it ..

---
+++ DSKY/Previews.swift
@@ -0,0 +1,18 @@
+//
+//  Previews.swift
+//  ApoDisKey
+//
+//  Created by Gavin Eadie on 11/28/24.
+//
+
+import SwiftUI
+
+struct Previews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Previews()
+}
